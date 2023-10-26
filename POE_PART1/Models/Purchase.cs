5783a7b4f48afb47e32af18f3b6ca3047be00c88
Pitsi Nkoana <ST10102652@rcconnect.edu.za>
2023-10-26 16:42:05 +0000

    updated purchase class

--- POE_PART1/Models/Purchase.cs
+++ POE_PART1/Models/Purchase.cs
@@ -2,5 +2,9 @@
 {
     public class Purchase
     {
+        public int PurchaseID { get; set; }
+        public string Purchase_Item { get;set; }
+        public int Number_Of_Iteams { get; set; }
+        public DateOnly Date { get; set; }
     }
 }
